075324e4323fd69aee83d4233cf600aeb4cd3873
Prash9 <prashantkamath94@gmail.com>
2018-02-04 00:32:59 +0530

    solved rrror in urls file

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
-from django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')),
       
 ]
