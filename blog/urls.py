b74157972dfb5201eb3fe145d3c714d2a4695905
Prash9 <prashantkamath94@gmail.com>
2018-02-18 16:03:26 +0530

    added views to create/edit blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url('^$', views.post_list, name='post_list'),
-    url('^post_detail/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
+    url('^post/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
+    url('^post/new/$',views.post_new,name='post_new'),
+    url('^post/edit/(?P<pk>\d+)/$',views.post_edit,name='post_edit'),
 ]
